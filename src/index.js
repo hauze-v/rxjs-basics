6ce43ea13b4ecd2a275bfa33d8f728d556b0a1a9
hauze-v <hauze_v@protonmail.com>
2023-02-25 10:53:17 -0700

    finished learning about flattening operators

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import { from, of, fromEvent, interval } from 'rxjs';
-import { map, pluck, filter, reduce, take, scan, tap } from 'rxjs/operators'
+import { mergeMap, switchMap, concatMap, pluck, filter, reduce, take, scan, exhaustMap, tap } from 'rxjs/operators'
+import { ajax } from 'rxjs/ajax';
 
-const observable = interval(500).pipe(
-  take(5),
-  tap({
-    next(val) {
-      console.log(val);
-    }
-  }),
-  reduce(
-    (accumulator, value) => accumulator + value,
-    0
-  )
+const button = document.querySelector('#btn');
+const observable = fromEvent(
+  button, 'click' 
+).pipe(
+  exhaustMap(() => {
+    return ajax.getJSON('https://jsonplaceholder.typicode.com/todos/1').pipe(
+      take(5),
+      tap({
+        complete() {
+          console.log('inner observable completed');
+        }
+      }),
+    )
+  })
 )
 
 // Value is the data emitted by observable. Subscribe takes in an Observer obj and must implement next function
 const subscription = observable.subscribe({
   next(value) {
-    console.log(value)
+    console.log(value);
   },
   complete() {
     console.log('complete');
